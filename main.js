abf450f074736632f6a00d62f13623c7bcdae8f2
John Fleming <johndeclanfleming@gmail.com>
2020-01-25 14:02:18 -0600

    finished initial page structure, styles, and project cards array loop functions

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 console.log('hi');
+
+const projects = [{
+    title: "Cool Project", 
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+    description: "This is the best project", 
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/John-Fleming/personal-bio-site", 
+    githubUrl: "https://github.com/John-Fleming/personal-bio-site"
+  }];
+
+const printToDom = (divId, textToPrint) => {
+    let selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+const createProjectCards = () => {
+    let domString = '';
+    for (project in projects) {
+        if (projects[project].available) {
+            domString += `<div class="project-cards">`;
+            domString +=    `<h1 class="project-title">${projects[project].title}</h1>`;
+            domString += `</div>`;  
+        }
+    }
+    printToDom('projectsPage', domString);   
+};
+
+createProjectCards();
