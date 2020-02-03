a0f5719783a5d9a0773a832085e67520f11ade4a
John Fleming <johndeclanfleming@gmail.com>
2020-02-02 22:49:21 -0600

    linked bootstrap and updated styles

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const projects = [{
-    title: "Cool Project", 
-    screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
-    description: "This is the best project", 
+    title: "Spotify", 
+    screenshot: "./assets/Spotify_Screenshot.png", 
+    description: "Spotify is one of the world's leading streaming platforms that I 100% built all by myself", 
     technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
     available: true,
-    url: "https://www.google.com", 
+    url: "https://www.spotify.com/", 
     githubUrl: "https://github.com/John-Fleming/personal-bio-site"
   }];
 
@@ -17,15 +17,22 @@ const createProjectCards = (arr) => {
     let domString = '';
     for (let i = 0; i < arr.length; i++) {
         if (arr[i].available) {
-            domString += `<div class="project-cards">`
-            domString +=     `<img class="project-screenshot" src=${arr[i].screenshot} alt="screenshot of ${arr[i].title}">`
-            domString +=     `<h1 class="project-title">${arr[i].title}</h1>`
-            domString +=     `<h3 class="technologies-used">${arr[i].technologiesUsed}</h3>`
-            domString +=     `<p class="project-description">${arr[i].description}</p>`
-            domString +=     `<div class="view-project-buttons">`
-            domString +=         `<button class="project-Url-button"><a href=${arr[i].url}>View Project</a></button>`
-            domString +=        `<button class="github-Url-button"><a href=${arr[i].githubUrl}>View Project on GitHub</a></button>`
-            domString +=     `</div>`
+            domString += `<div class="container">`;
+            domString += `<div class="row">`;
+            domString += `<div class="project-cards col-md-12">`;
+            domString += `<div class="project-screenshot">`;
+            domString +=     `<img src=${arr[i].screenshot} alt="screenshot of ${arr[i].title}">`;
+            domString += `</div>`;
+            domString += `<div class="project-info text-center">`;
+            domString +=     `<h1 class="project-title">${arr[i].title}</h1>`;
+            domString +=     `<h5 class="technologies-used">${arr[i].technologiesUsed}</h5>`;
+            domString +=     `<p class="project-description mt-5 mb-5">${arr[i].description}</p>`;
+            domString +=     `<div class="project-btns">`;
+            domString +=        `<button class="project-Url-button btn btn-dark btn-lg mr-2"><a class="text-white" href=${arr[i].url} target="_blank">View Project</a></button>`;
+            domString +=        `<button class="github-Url-button btn btn-dark btn-lg ml-2"><a class="text-white" href=${arr[i].githubUrl} target="_blank">View on GitHub</a></button>`;
+            domString +=     `</div>`;
+            domString += `</div>`;
+            domString += `</div>`;  
             domString += `</div>`;  
         }
     }
