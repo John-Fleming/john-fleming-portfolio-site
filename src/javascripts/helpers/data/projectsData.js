a3e7d7c4f42cc907b56a7e9aa326e4214309aaf6
John Fleming <johndeclanfleming@gmail.com>
2020-04-05 14:46:25 -0500

    axios request printing projects array

--- src/javascripts/helpers/data/projectsData.js
+++ src/javascripts/helpers/data/projectsData.js
@@ -1,53 +1,20 @@
-const projects = [{
-  title: 'Chatty App',
-  screenshot: './assets/chatty_screenshot.png',
-  // eslint-disable-next-line max-len
-  description: 'The chatty app is a messaging app I contributed to for a group project at NSS. It allows a user to select who is sending a message, submit a new message, and perform specific actions such as liking and disliking messages, deleting messages, and selecting app styles.',
-  technologiesUsed: 'HTML, CSS, Sass, Bootstrap, JavaScript, jQuery',
-  available: true,
-  url: '',
-  githubUrl: 'https://github.com/John-Fleming/chatty-venus-fly-traps',
-},
-{
-  title: 'Tamagotchi',
-  screenshot: './assets/tamagotchi_screenshot.png',
-  // eslint-disable-next-line max-len
-  description: 'Modelled after the 90\'s Toy, the Tamagotchi project was a NSS class assignment to practice modular development and styling using Sass. Users can alter the scores calculating when clicking buttons in each displayed quadrant.',
-  technologiesUsed: 'HTML, CSS, Sass, JavaScript, jQuery',
-  available: true,
-  url: '',
-  githubUrl: 'https://github.com/John-Fleming/tamagotchi',
-},
-{
-  title: 'Spotify',
-  screenshot: './assets/Spotify_Screenshot.png',
-  description: 'Spotify is one of the world\'s leading streaming platforms that I 100% built all by myself',
-  technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-  available: true,
-  url: 'https://www.spotify.com/',
-  githubUrl: 'https://github.com/John-Fleming/personal-bio-site',
-},
-{
-  title: 'Spotify2',
-  screenshot: './assets/Spotify_Screenshot.png',
-  description: 'A better Spotify!',
-  technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-  available: true,
-  url: 'https://www.spotify.com/',
-  githubUrl: 'https://github.com/John-Fleming/personal-bio-site',
-},
-{
-  title: 'Spotify3',
-  screenshot: './assets/Spotify_Screenshot.png',
-  description: 'This is my final form.',
-  technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-  available: true,
-  url: 'https://www.spotify.com/',
-  githubUrl: 'https://github.com/John-Fleming/personal-bio-site',
-},
-];
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
 
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
-const getProjects = () => projects;
-
+const getProjects = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/projects.json`)
+    .then((response) => {
+      const allProjects = response.data;
+      const availableProjects = [];
+      Object.keys(allProjects).forEach((project) => {
+        if (allProjects[project].available) {
+          availableProjects.push(allProjects[project]);
+        }
+      });
+      resolve(availableProjects);
+    })
+    .catch((err) => reject(err));
+});
 export default { getProjects };
