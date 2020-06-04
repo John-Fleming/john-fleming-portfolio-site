604a706528b1cc98516fe0de6a9572f82b73ef1d
John Fleming <johndeclanfleming@gmail.com>
2020-06-03 22:53:47 -0500

    abandoned flipping due to bug

--- src/javascripts/components/projectCard/projectCard.js
+++ src/javascripts/components/projectCard/projectCard.js
@@ -2,17 +2,10 @@ const buildProjectCard = (project) => {
   const techUsed = project.technologiesUsed.split(', ');
   let domString = '';
   domString += '<div class="col-md-6 mb-5 d-flex justify-content-center">';
-  domString += '<div class="single-project-container">';
   domString += '<div class="card project-card project-details mx-auto">';
-  // domString += `<div class="front" style="background-image: url(${project.screenshot})">`;
-  domString += '<div class="front">';
-  // domString += `<div class="project-screenshot" style="background-image: url(${project.screenshot})></div>`;
-  // domString += `<div class=""><img class="project-screenshot card-img-top" src=${project.screenshot} alt="project-screenshot"></div>`;
   domString += `<img class="project-screenshot card-img-top" src=${project.screenshot} alt="project-screenshot">`;
-  domString += `<div class="card-body view-project d-flex justify-content-center"><button class="my-button view-project-btn">${project.title}</button></div></div>`;
-  domString += '<div class="back">';
-  domString += `<div class="card-header"><h4 class="py-1">${project.title}</h4></div>`;
   domString += '<div class="card-body">';
+  domString += `<h4 class="py-1">${project.title}</h4>`;
   domString += `<p>${project.description}</p>`;
   domString += '<div class="project-btns">';
   domString += `<button class="project-demo-btn my-button grow mt-1 mx-3"><a href=${project.url} target="_blank">Demo</a></button>`;
@@ -26,8 +19,6 @@ const buildProjectCard = (project) => {
     domString += `<li>${tech}</li>`;
   });
   domString += '</ul>';
-  domString += '</div>';
-  domString += '</div>';
   domString += '</div>';
   domString += '</div>';
   domString += '</div>';
--- src/javascripts/components/projects/projects.js
+++ src/javascripts/components/projects/projects.js
@@ -2,21 +2,6 @@ import utils from '../../helpers/utils';
 import projectData from '../../helpers/data/projectsData';
 import projectCard from '../projectCard/projectCard';
 
-const buttonHoverEvent = (e) => {
-  const targetedButton = e.target.closest('button');
-  $(targetedButton).addClass('hover-state');
-};
-
-const buttonHoverExitEvent = (e) => {
-  const targetedButton = e.target.closest('button');
-  $(targetedButton).removeClass('hover-state');
-};
-
-// const flipCard = (e) => {
-//   const targetedCard = e.target.closest('.card');
-//   $(targetedCard).css(transform: rotateY(180deg));
-// };
-
 const printProjects = () => {
   projectData.getProjects()
     .then((projects) => {
@@ -27,9 +12,6 @@ const printProjects = () => {
       });
       domString += '</div>';
       utils.printToDom('projects', domString);
-      $('body').on('mouseenter', '.my-button', buttonHoverEvent);
-      $('body').on('mouseleave', '.my-button', buttonHoverExitEvent);
-      $('body').on('click', '.view-project-btn', flipCard);
     })
     .catch((err) => console.error(err));
 };
